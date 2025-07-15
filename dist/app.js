4bb2cb4a424db2235ee758098206994112399170
Alamin <alamin.kzs06@gmail.com>
2025-07-15 21:58:55 +0600

    Middleware added

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express_1.default.json());
+const filePath = path_1.default.join(__dirname, "../db/todo.json");
+app.get("/", (req, res) => {
+    res.send("Welcome To Todos App!");
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(data);
+    res.json(data);
 });
+app.post("/todos/create-todo", (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.json("Hello World!");
+});
+exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
-import express, { Application, Request, Response } from 'express'
-const app : Application = express()
+import express, { Application, Request, Response } from "express";
+import fs from "fs";
+import path from "path";
+const app: Application = express();
 
+app.use(express.json());
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
-})
+const filePath = path.join(__dirname, "../db/todo.json");
 
-app.post('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome To Todos App!");
+});
 
+app.get("/todos", (req: Request, res: Response) => {
+  const data = fs.readFileSync(filePath, { encoding: "utf-8" });
+  console.log(data);
+  res.json(data);
+});
+
+app.post("/todos/create-todo", (req: Request, res: Response) => {
+  const { title, body } = req.body;
+  console.log(title, body);
+  res.json("Hello World!");
+});
 
 export default app;
